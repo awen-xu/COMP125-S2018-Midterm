63eef5e4aa690931d9db032e71211b2a98e1aade
awen-xu <wxu59@my.centennialcollege.ca>
2018-06-20 11:58:22 -0400

    added smooth scrolling

--- Scripts/app.js
+++ Scripts/app.js
@@ -140,14 +140,56 @@ let app;
         });
     }
 
+
+
+    // this allows for smooth scrolling
+    // code from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    function SmoothScroll() {
+        // Select all links with hashes
+        $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function (event) {
+                // On-page links
+                if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                    &&
+                    location.hostname == this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    let target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000, function () {
+                            // Callback after animation
+                            // Must change focus!
+                            let $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            };
+                        });
+                    }
+                }
+            });
+    }
+
     // start up function for organization
     function Start() {
         console.log(`%cApp Started!`, "font-weight: bold; font-size: 16px; color: maroon; background-color: rgba(0, 255, 0, 0.3);")
 
         TourParagraphs();
-
         ContactContent();
-
+        SmoothScroll();
     }
 
     window.addEventListener("load", Start);
